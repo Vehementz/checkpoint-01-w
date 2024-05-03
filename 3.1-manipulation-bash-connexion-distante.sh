8f61138dd061d9b36a36a3876a85db6ece600d32
Vehementz <vale-tudo91@sfr.fr>
2024-05-03 11:29:50 +0200

    quelques questions de la partie 4 ont été répondu

--- 3.1-manipulation-bash-connexion-distante.sh
+++ 3.1-manipulation-bash-connexion-distante.sh
@@ -2,7 +2,7 @@
 # Développer un script Bash qui permet de :
 # Se connecter en ssh à ton conteneur via le compte "wilder" en ne demandant aucune information à l'utilisateur qui l'exécute
 
-# sshpass peux être utilisé 
+# sshpass peux être utilisé, si sshpass n'est pas installé il peux être installé avec la commande ci-dessous
 # apt-get install sshpass
 
 ipaddress="176.31.160.40"
---
+++ script.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+MAX=95
+EMAIL=wilder@email.sh
+
+USE=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}')
+if [ $USE -gt $MAX ]; then
+	echo "Percent used: $USE" | mail -s "Running out of CPU power" $EMAIL
+fi
